62b393571bd4d76d3125d9ef9e919c910c6246a4
caraciol <caraciol@ec3d9ad2-a13d-11dd-988f-d3ef9af6d102>
2008-11-06 01:48:10 +0000

    Version 0.5

--- src/lib/pyschart.py
+++ src/lib/pyschart.py
@@ -23,24 +23,26 @@ from appuifw import *
 import key_codes
 
 # linechart.py - Line Chart Plotter for PyS60
-# @version 0.4
-# @date:  26/10/2008
+# @version 0.5
+# @date:  05/11/2008
 
 class LineChart:
     
     ##The constructor.
     # @param canvas: The graphics component
     # @param xyrange: The list with x and y range [min_x,max_x,step_x,min_y,max_y,step_y]
+    # @param xs: The list with x series 
+    # @param ys: The list with the y series     
     # @param subtitle: the subtitle for x axe
+    # @param color: The color used to fill the line
     # @param colorBack: The background color
     # @param formatter: The format of the series composed in the chart
-    def __init__(self,canvas,xyrange,subtitle="",
+    def __init__(self,canvas,xyrange,xs,ys=None,subtitle="", color = (255,0,0),
                   colorBack=(255,255,255),formatter=lambda x:x):
         self._view = canvas
         self._view.bind(key_codes.EKeyLeftArrow,lambda: self._traverse(1))
         self._view.bind(key_codes.EKeyRightArrow,lambda: self._traverse(0))
         self._width,self._height = self._view.size
-        self._img = Image.new((self._width,self._height))
         self._position = None
         self._min_x = None
         self._min_y = None
@@ -49,8 +51,15 @@ class LineChart:
         self._scale_y = None
         self._axe_x = 0
         self._actual_pos = 0
-        self._setPosition(xyrange)
-        self._set_axes(xyrange,colorBack,formatter)
+        self._xyrange = xyrange
+        self._colorBack = colorBack
+        self._color = color
+        self._formatter = formatter
+        self._xs = xs
+        self._ys = ys
+        self._setPosition()
+        self._set_axes()
+        self._plot()
 
 
     ##Callback method for key events (navigates into the chart)
@@ -59,16 +68,20 @@ class LineChart:
         if code: 
             if self._actual_pos > 0: 
                 self._actual_pos -= 1
+                self._set_axes()
+                self._plot()
+              
                 
         #right
         else:
             if self._actual_pos < self._axe_x + 1:
                 self._actual_pos += 1
-               
+                self._set_axes()
+                self._plot()
 
     ##Defines the ranges of the graph in the screen.
-    def _setPosition(self,xyrange):
-        left = self._view.measure_text(unicode(float(xyrange[4])), font=('normal',10,FONT_BOLD))[1]
+    def _setPosition(self):
+        left = self._view.measure_text(unicode(float(self._xyrange[4])), font=('normal',10,FONT_BOLD))[1]
         self._position = [left+2,self._height-40,self._width-10,10]
         
      
@@ -92,22 +105,19 @@ class LineChart:
      
      ## Plot chart axes.
       # @param self The object pointer.
-      # @param xyrange: The list with x and y range [min_x,max_x,step_x,min_y,max_y,step_y]
-      # @param colorBack: The color used to fill the chart
-      # @param formatter: The format of the numbers in the chart
-    def _set_axes(self,xyrange,colorBack,formatter):
+    def _set_axes(self):
         #position: The position where the chart will be painted [left,bottom,right,top]
         left,bottom,right,top = self._position
-        self._min_x,max_x,step_x,self._min_y,max_y,step_y = xyrange
+        self._min_x,max_x,step_x,self._min_y,max_y,step_y = self._xyrange
         self._scale_x = float(right - left)/(max_x-self._min_x)
         self._scale_y = float(bottom - top)/(max_y-self._min_y)
         self._view.clear()
-        self._view.rectangle([(left,top),(right+1,bottom+1)],0,fill = colorBack)
+        self._view.rectangle([(left,top),(right+1,bottom+1)],0,fill = self._colorBack)
         self._axe_x = 0
         for x in self._arange(self._min_x,max_x,step_x):
 
             if self._axe_x == self._actual_pos:
-                color_line = (255,0,0)            
+                color_line = self._color        
             else:
                 color_line = (0,0,0)
             
@@ -121,15 +131,15 @@ class LineChart:
 
             if self._axe_x == self._actual_pos:
                 tl = (left+self._scale_x*(x-self._min_x)-5, top+7)
-                bbox = self._view.measure_text(unicode(formatter(x)), font=('normal',10,FONT_BOLD))[0]
+                bbox = self._view.measure_text(unicode(self._formatter(x)), font=('normal',10,FONT_BOLD))[0]
                 t = (tl[0]-bbox[0],tl[1]-bbox[1])
                 self._view.rectangle((t[0]+bbox[0]-4,t[1]+bbox[1]-4,t[0]+bbox[2]+4,t[1]+bbox[3]+4),outline=(0,0,0), fill = (255,255,255))                       
-                self._view.text((left+self._scale_x*(x-self._min_x)-5, top+15), unicode(formatter(x)), font= ('normal',10,FONT_BOLD))
+                self._view.text((left+self._scale_x*(x-self._min_x)-5, top+15), unicode(self._formatter(x)), font= ('normal',10,FONT_BOLD))
             
             self._axe_x +=1
             
         for y in self._arange(self._min_y,max_y,step_y):
-            self._view.text((2,bottom+2-self._scale_y*(y-self._min_y)), unicode(formatter(y)), font= ('normal',10,FONT_BOLD))
+            self._view.text((2,bottom+2-self._scale_y*(y-self._min_y)), unicode(self._formatter(y)), font= ('normal',10,FONT_BOLD))
             for i in range(left,right,3):            
                 self._view.point((i,bottom-self._scale_y*(y-self._min_y)), 0)
                 self._view.point((i+1,bottom-self._scale_y*(y-self._min_y)), 0)   
@@ -140,24 +150,21 @@ class LineChart:
             
             
     ## Plot the real graph
-        #  @param self The object pointer.  
-        #  @param xs: The list with x series 
-        #  @param ys: The list with the y series
-        #  @param color: The color used to fill the line     
-    def plot(self,xs,ys=None,color = (255,0,0)):
-        if ys == None:
-            ys = xs
-            xs = range(len(ys))
+     #  @param self The object pointer.  
+    def _plot(self):
+        if self._ys == None:
+            self._ys = self._xs
+            self._xs = range(len(self._ys))
         left,bottom,right,top = self._position
-        last = left + (xs[0] - self._min_x) * self._scale_x, bottom - (ys[0]-self._min_y) * self._scale_y    
-        for i in range(1, len(ys)):
-            p = left + (xs[i] - self._min_x) * self._scale_x, bottom - (ys[i]-self._min_y) * self._scale_y 
-            self._view.line([last, p], color)
+        last = left + (self._xs[0] - self._min_x) * self._scale_x, bottom - (self._ys[0]-self._min_y) * self._scale_y    
+        for i in range(1, len(self._ys)):
+            p = left + (self._xs[i] - self._min_x) * self._scale_x, bottom - (self._ys[i]-self._min_y) * self._scale_y 
+            self._view.line([last, p], self._color)
             last = p
-        self._view.point(last,color)
+        self._view.point(last,self._color)
 
         #The subtitle
-        self._view.rectangle([(2,bottom+17),(15,self._height-4)],0,fill = color )
+        self._view.rectangle([(2,bottom+17),(15,self._height-4)],0,fill = self._color )
         self._view.text((17,self._height-5), self._subtitle, font= ('normal',13))
        
             
