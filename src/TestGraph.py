25ba45cc23070bae2cbfb887a7acc781d9f3fe6a
caraciol <caraciol@ec3d9ad2-a13d-11dd-988f-d3ef9af6d102>
2008-11-07 03:12:04 +0000

    Version 0.6 - Line Chart Beta Version

--- src/TestGraph.py
+++ src/TestGraph.py
@@ -1,3 +1,29 @@
+""" 
+    Copyright 2008 Marcel Pinheiro Caraciolo <caraciol@gmail.com>
+ 
+    This file is part of PySChart.
+
+    PySChart is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Lesser Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    PySChart is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Lesser Public License for more details.
+
+    You should have received a copy of the GNU General Lesser Public License
+    along with PySChart; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+"""
+
+"""  Mini application to test the charts.
+     Author: Marcel P. Caraciolo    """
+
+
+
+
 from appuifw import *
 import e32
 from pyschart import LineChart
@@ -7,12 +33,31 @@ def quit():
     print "Exit key pressed"
     app_lock.signal()
 
+def line_example01():
+    
+    chart = LineChart(app.body.size,[0,125,25,0,90,22.5], [0.0,22.0,50.0,62.0,70.0,85.0],[0.0,25.0,50.0,30.0,20.0,80.0], subtitle="Producao", colorBack = (128,128,128))   
+  
+
+def line_example02():
+    chart = LineChart(app.body.size,[0,125,25,0,125,25], [0.0,22.0,50.0,62.0,70.0,85.0],[0.0,25.0,50.0,30.0,20.0,80.0] , subtitle="Producao", legend = ["Jan","Feb","Mar"], colorBack = (128,128,128))  
+
+def pizza_chart():
+    pass
+
+
+def columns_chart():
+    pass
+
 
 canvas = Canvas()
 #app.screen = "full"
 app.body = canvas
-chart = LineChart([0,125,25,0,125,25], [0.0,25.0,50.0],[0.0,25.0,50.0], subtitle="Producao")   
-    
+
+app.menu=[(u"Pizza Chart", pizza_chart),
+                  (u"Columns Chart", columns_chart),
+                  (u"Line Chart", ((u"Example 01", line_example01),
+                               (u"Example 02", line_example02)))]
+
 app.exit_key_handler = quit
 app_lock = e32.Ao_lock()
 app_lock.wait()
--- src/lib/pyschart.py
+++ src/lib/pyschart.py
@@ -22,36 +22,39 @@ import graphics, appuifw, key_codes
 
 
 # linechart.py - Line Chart Plotter for PyS60
-# @version 0.51
+# @version 0.6
 # @date:  06/11/2008
 
 class LineChart:
-    
-    ##The constructor.
+
+
+    ##Creates a new instance of LineChart.
+    # @param size  Chart's size (width,height)
     # @param xyrange The list with x and y range [min_x,max_x,step_x,min_y,max_y,step_y]
-    # @param xs The list with x series 
+    # @param xs The list with values of data  x series
     # @param ys The list with the y series     
-    # @param subtitle the subtitle for x axe
-    # @param color The color used to fill the line
+    # @param subtitle identification of the data set
+    # @param legend labels of data
+    # @param color The color of the line
     # @param colorBack The background color
     # @param formatter The format of the series composed in the chart
-    def __init__(self,xyrange,xs,ys=None,subtitle="", color = (255,0,0),
+    def __init__(self,size,xyrange,xs,ys=None,subtitle="", legend = [], color = (255,0,0),
                   colorBack=(255,255,255),formatter=lambda x:x):
 
-        self._img = graphics.Image.new((0,0))
+        self._img = graphics.Image.new(size)
         self._view = appuifw.Canvas(redraw_callback= self._OnUpdate)
         appuifw.app.body = self._view
         
         self._view.bind(key_codes.EKeyLeftArrow,lambda: self._traverse(1))
         self._view.bind(key_codes.EKeyRightArrow,lambda: self._traverse(0))
 
-        self._img = graphics.Image.new(self._view.size)
         self._width,self._height = self._img.size        
         self._position = None
         self._min_x = None
         self._min_y = None
         self._scale_x = None
         self._subtitle = unicode(subtitle)
+        self._legend = legend
         self._scale_y = None
         self._axe_x = 0
         self._actual_pos = 0
@@ -79,7 +82,7 @@ class LineChart:
                 
         #right
         else:
-            if self._actual_pos < self._axe_x:
+            if self._actual_pos < len(self._xs)-1:
                 self._actual_pos += 1
                 self._set_axes()
                 self._plot()
@@ -126,30 +129,18 @@ class LineChart:
         
         self._img.clear()
         self._img.rectangle([(left,top),(right+1,bottom+1)],0,fill = self._colorBack)
+
         self._axe_x = 0
         
-        for x in self._arange(self._min_x,max_x,step_x):
-
-            if self._axe_x == self._actual_pos:
-                color_line = self._color        
-            else:
-                color_line = (0,0,0)
-            
-            #self._view.text((15+self._scale_x*(x-self._min_x), self._height-24), unicode(formatter(x)), font= ('normal',10,FONT_BOLD))
+        
+        for x in self._arange(self._min_x,max_x,step_x):         
+            self._img.text((15+self._scale_x*(x-self._min_x), self._height-24), unicode(self._formatter(x)), font= ('normal',10,graphics.FONT_BOLD))
             for z in range(top,bottom,3):
-                self._img.point((left+self._scale_x*(x-self._min_x),z), outline = color_line)
-                self._img.point((left+self._scale_x*(x-self._min_x),z+1), outline = color_line)            
+                self._img.point((left+self._scale_x*(x-self._min_x),z),0)
+                self._img.point((left+self._scale_x*(x-self._min_x),z+1),0)            
             self._img.point((left+self._scale_x*(x-self._min_x), bottom-1), 0)
             self._img.point((left+self._scale_x*(x-self._min_x), top+1), 0)
 
-            if self._axe_x == self._actual_pos:
-                tl = (left+self._scale_x*(x-self._min_x)-5, top+7)
-                bbox = self._view.measure_text(unicode(self._formatter(x)), font=('normal',10,graphics.FONT_BOLD))[0]
-                t = (tl[0]-bbox[0],tl[1]-bbox[1])
-                self._img.rectangle((t[0]+bbox[0]-4,t[1]+bbox[1]-4,t[0]+bbox[2]+4,t[1]+bbox[3]+4),outline=(0,0,0), fill = (255,255,255))                       
-                self._img.text((left+self._scale_x*(x-self._min_x)-5, top+15), unicode(self._formatter(x)), font= ('normal',10,graphics.FONT_BOLD))
-            
-            self._axe_x +=1
             
         for y in self._arange(self._min_y,max_y,step_y):
             self._img.text((2,bottom+2-self._scale_y*(y-self._min_y)), unicode(self._formatter(y)), font= ('normal',10,graphics.FONT_BOLD))
@@ -158,7 +149,26 @@ class LineChart:
                 self._img.point((i+1,bottom-self._scale_y*(y-self._min_y)), 0)   
             self._img.point((left+1, bottom-self._scale_y*(y-self._min_y)), 0)
             self._img.point((right-1,bottom-self._scale_y*(y-self._min_y)), 0)
-            
+
+        for x in self._xs:
+            if self._axe_x == self._actual_pos:
+                for z in range(top,bottom,3):
+                    self._img.point((left+self._scale_x*(x-self._min_x),z), outline = self._color)
+                    self._img.point((left+self._scale_x*(x-self._min_x),z+1), outline = self._color)            
+                self._img.point((left+self._scale_x*(x-self._min_x), bottom-1), 0)
+                self._img.point((left+self._scale_x*(x-self._min_x), top+1), 0)
+
+                tl = (left+self._scale_x*(x-self._min_x)-5, top+7)
+                if self._legend:
+                    legend = unicode(self._legend[self._axe_x] + ": ") + unicode(self._formatter(x)) + "," + unicode(self._formatter(self._ys[self._actual_pos]))
+                else:
+                    legend = unicode(self._formatter(x)) + "," + unicode(self._formatter(self._ys[self._actual_pos]))
+                bbox = self._view.measure_text(legend, font=('normal',10,graphics.FONT_BOLD))[0]
+                t = (tl[0]-bbox[0],tl[1]-bbox[1])
+                self._img.rectangle((t[0]+bbox[0]-4,t[1]+bbox[1]-4,t[0]+bbox[2]+4,t[1]+bbox[3]+4),outline=(0,0,0), fill = (255,255,255))
+                self._img.text((left+self._scale_x*(x-self._min_x)-5, top+15), legend, font= ('normal',10,graphics.FONT_BOLD))
+            self._axe_x +=1
+        
        
             
             
